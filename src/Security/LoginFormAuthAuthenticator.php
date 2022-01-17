de10361e764e7f48cc99898a0172a7586aa30733
LahcenLamkirich <lahcen.lamkirich@gmail.com>
2022-01-17 22:21:26 +0100

    commit

--- src/Security/LoginFormAuthAuthenticator.php
+++ src/Security/LoginFormAuthAuthenticator.php
@@ -50,7 +50,7 @@ class LoginFormAuthAuthenticator extends AbstractLoginFormAuthenticator
         }
 
         // For example:
-        return new RedirectResponse($this->urlGenerator->generate('admin'));
+        return new RedirectResponse($this->urlGenerator->generate('home'));
         throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
     }
 
